60de743ecd8e4b4447dc8fe440b4a0594e83e9b6
Andre <andrevitorlopes@gmail.com>
2022-11-15 16:10:44 +0100

    Deploy to Heroku

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import homeRoutes from './endpoints/routes/home';
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
@@ -12,6 +12,6 @@ app.use(homeRoutes);
 
 app.use(exerciseRoutes);
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
