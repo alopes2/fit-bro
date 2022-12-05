4b8cecf9e26bfeaad9da7bf360f350de8efb6b0d
Andre <andrevitorlopes@gmail.com>
2022-12-05 14:00:02 +0100

    Add base to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,25 +8,17 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <img src="/favicon-96.png" className="logo" alt="Vite logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Welcome to FitBuddy</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Under construction...
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
