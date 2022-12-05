f80a7181fa49130dbc995653fc3fa6d2152a8124
Andre <andrevitorlopes@gmail.com>
2022-12-05 16:33:48 +0100

    Add cors to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import cors from 'cors';
 import trainingsRoutes from './endpoints/routes/trainings';
 import homeRoutes from './endpoints/routes/home';
 
@@ -11,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors())
 
 app.use(homeRoutes);
 app.use(trainingsRoutes);
@@ -18,3 +20,4 @@ app.use(trainingsRoutes);
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
